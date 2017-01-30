c75a7f5e0acb7b395aed37dc3c855d385026fd95
shayBarelOz <shikooz@icloud.com>
2017-01-30 18:16:23 +0200

    asd

---
+++ routes/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var paypal = require('paypal-rest-sdk');
+var config = {};
+
+// Routes
+
+exports.index = function (req, res) {
+  res.render('index');
+};
+
+exports.create = function (req, res) {
+    var method = req.param('method');
+
+	var payment = {
+		"intent": "sale",
+		"payer": {
+		},
+		"transactions": [{
+			"amount": {
+                "total": "2.00",
+                "currency": "USD"
+			},
+            "description": "My awesome payment"
+		}]
+	};
+
+	if (method === 'paypal') {
+		payment.payer.payment_method = 'paypal';
+		payment.redirect_urls = {
+			"return_url": "http://localhost:3000/execute",
+			"cancel_url": "http://localhost:3000/cancel"
+		};
+	} else if (method === 'credit_card') {
+		var funding_instruments = [
+			{
+				"credit_card": {
+					"type": req.param('type').toLowerCase(),
+					"number": req.param('number'),
+					"expire_month": req.param('expire_month'),
+					"expire_year": req.param('expire_year'),
+					"first_name": req.param('first_name'),
+					"last_name": req.param('last_name')
+				}
+			}
+		];
+		payment.payer.payment_method = 'credit_card';
+		payment.payer.funding_instruments = funding_instruments;
+	}
+
+	paypal.payment.create(payment, function (error, payment) {
+		if (error) {
+			console.log(error);
+			res.render('error', { 'error': error });
+		} else {
+			req.session.paymentId = payment.id;
+			// res.render('create', { 'payment': payment });
+            var redirectUrl;
+            for(var i=0; i < payment.links.length; i++) {
+                var link = payment.links[i];
+                if (link.method === 'REDIRECT') {
+                    redirectUrl = link.href;
+                }
+            }
+            res.redirect(redirectUrl);
+		}
+	});
+};
+
+exports.execute = function (req, res) {
+	var paymentId = req.session.paymentId;
+	var payerId = req.param('PayerID');
+
+	var details = { "payer_id": payerId };
+	var payment = paypal.payment.execute(paymentId, details, function (error, payment) {
+		if (error) {
+			console.log(error);
+			res.render('error', { 'error': error });
+		} else {
+			res.render('execute', { 'payment': payment });
+		}
+	});
+};
+
+exports.cancel = function (req, res) {
+  res.render('cancel');
+};
+
+// Configuration
+
+exports.init = function (c) {
+	config = c;
+	paypal.configure(c.api);
+};
